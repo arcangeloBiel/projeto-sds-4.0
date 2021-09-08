1ed0af9f484e0f9133135ad7754486dac4df1c64
arcangeloBiel <joaoarcangelo7@hotmail.com>
2021-09-07 21:16:26 -0300

    primeiro commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+import DataTable from './components/DataTable';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <>
+    <NavBar />
+    <div className="container">
+      <h1 className="text-primary">hello word</h1>
+      <DataTable />
+    </div>
+    <Footer />
+    </>
+  );
+}
+
+export default App;
