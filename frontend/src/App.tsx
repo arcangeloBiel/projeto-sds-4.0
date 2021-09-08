69a56adcc86d09979a773cd204a09afcd1a83845
arcangeloBiel <joaoarcangelo7@hotmail.com>
2021-09-08 07:20:03 -0300

    cria graficos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,27 @@ import './App.css';
 import DataTable from './components/DataTable';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
+import BarCharts from './components/BarCharts';
 
 function App() {
   return (
     <>
     <NavBar />
     <div className="container">
-      <h1 className="text-primary">hello word</h1>
+      <h1 className="text-primary py-3">DashBoard de venda</h1>
+<div className="row px-3">
+  <div className="col-sm-6">
+    <h5>Taxa de sucesso(%)</h5>
+    <BarCharts />
+  </div>
+
+  <div className="col-sm-6">
+    <h5>Todas as vendas</h5>
+    <BarCharts />
+  </div>
+
+</div>
+
       <DataTable />
     </div>
     <Footer />
